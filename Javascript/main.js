bf454333aeb32ea670a7a555edbcf6b95442dc33
JaeH-Y <yjh21175753@gmail.com>
2025-11-06 09:01:27 +0900

    이미지 슬라이더 CSS, JS  부트스트랩 CSS, JS 추가, about-skill-project 구조 설계,CSS 구성 및 컬러 조합 변경

--- Javascript/main.js
+++ Javascript/main.js
@@ -16,3 +16,22 @@ const swiper = new Swiper('.presentwork .swiper', {
     prevEl: '.presentwork .swiper-button-prev',
   },
 });
+
+const swiper2 = new Swiper('.projects .swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.projects .swiper-pagination',
+    clickable : true,
+    type : "bullets",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.projects .swiper-button-next',
+    prevEl: '.projects .swiper-button-prev',
+  },
+});
